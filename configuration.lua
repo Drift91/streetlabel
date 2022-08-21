525c352ed66a41f913f311f5104762c92bcede24
Kyle J. McKeown <mckeown_k@hotmail.com>
2022-08-21 16:49:54 -0400

    Further size and position adjustment

--- configuration.lua
+++ configuration.lua
@@ -1,8 +1,8 @@
 config = {
 	color = {r = 240, g = 200, b = 80},
 	position = {
-		x = 17.55, -- distance across the x-axis, from left to right (default: 17.55)
-		y = 3,	   -- distance across the y-axis, from bottom to top (default: 3)
+		x = 16.45, -- distance across the x-axis, from left to right (default: 17.55)
+		y = 1.25,	   -- distance across the y-axis, from bottom to top (default: 3)
 	},
-	vehicleCheck = true,
+	vehicleCheck = false,
 }
